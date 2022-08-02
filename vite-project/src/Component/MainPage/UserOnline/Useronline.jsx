103bcec629aaafaba3253d6e8ebeab0e148f85d3
Nutchawakorn NaLampang <nutchawakorn.n@gmail.com>
2022-08-02 14:03:51 +0700

    Useronlinecss

--- vite-project/src/Component/MainPage/UserOnline/Useronline.jsx
+++ vite-project/src/Component/MainPage/UserOnline/Useronline.jsx
@@ -5,6 +5,23 @@ import ListItem from '@mui/material/ListItem'
 import ListItemText from '@mui/material/ListItemText'
 import ListItemAvatar from '@mui/material/ListItemAvatar'
 import Avatar from '@mui/material/Avatar'
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+
+//List
+// import ListSubheader from '@mui/material/ListSubheader';
+
+// import ListItemButton from '@mui/material/ListItemButton';
+// import ListItemIcon from '@mui/material/ListItemIcon';
+
+// import Collapse from '@mui/material/Collapse';
+// import InboxIcon from '@mui/icons-material/MoveToInbox';
+// import DraftsIcon from '@mui/icons-material/Drafts';
+// import SendIcon from '@mui/icons-material/Send';
+// import ExpandLess from '@mui/icons-material/ExpandLess';
+// import ExpandMore from '@mui/icons-material/ExpandMore';
+// import StarBorder from '@mui/icons-material/StarBorder';
+
 
 const users = [
   {
@@ -33,7 +50,7 @@ const users = [
 
 function Useronline() {
   return (
-    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+    <div className='Useronline'><List>
       {users.map((user, idx) =>
         <ListItem key={idx}>
           <ListItemAvatar>
@@ -42,8 +59,67 @@ function Useronline() {
           <ListItemText primary={user.name} />
         </ListItem>
       )}
+      <div className='Seemore'>
+        <Fab size="small" color="#46469C" aria-label="add">
+          <AddIcon />
+        </Fab>
+      </div>
     </List>
+    </div>
   )
 }
 
+
+// function NestedList() {
+//   const [open, setOpen] = React.useState(true);
+
+//   const handleClick = () => {
+//     setOpen(!open);
+//   };
+
+//   return (
+//     <List
+//       sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+//       component="nav"
+//       aria-labelledby="nested-list-subheader"
+//       subheader={
+//         <ListSubheader component="div" id="nested-list-subheader">
+//           Nested List Items
+//         </ListSubheader>
+//       }
+//     >
+//       <ListItemButton>
+//         <ListItemIcon>
+//           <SendIcon />
+//         </ListItemIcon>
+//         <ListItemText primary="Sent mail" />
+//       </ListItemButton>
+//       <ListItemButton>
+//         <ListItemIcon>
+//           <DraftsIcon />
+//         </ListItemIcon>
+//         <ListItemText primary="Drafts" />
+//       </ListItemButton>
+//       <ListItemButton onClick={handleClick}>
+//         <ListItemIcon>
+//           <InboxIcon />
+//         </ListItemIcon>
+//         <ListItemText primary="Inbox" />
+//         {open ? <ExpandLess /> : <ExpandMore />}
+//       </ListItemButton>
+//       <Collapse in={open} timeout="auto" unmountOnExit>
+//         <List component="div" disablePadding>
+//           <ListItemButton sx={{ pl: 4 }}>
+//             <ListItemIcon>
+//               <StarBorder />
+//             </ListItemIcon>
+//             <ListItemText primary="Starred" />
+//           </ListItemButton>
+//         </List>
+//       </Collapse>
+//     </List>
+//   );
+// }
+
 export default Useronline
+
